1a8bcfa87858f4645c88594613919c9bdb0ba1d7
Ryan Slominski <Ryan.Slominski@gmail.com>
2024-12-18 17:17:41 -0500

    Bump Keycloak to v26 (jlab-v2)

--- container/keycloak/initdb.d/03_client.sh
+++ container/keycloak/initdb.d/03_client.sh
@@ -11,3 +11,6 @@ KEYCLOAK_SERVICE_ACCOUNT_ENABLED=true
 KEYCLOAK_REDIRECT_URIS='["https://localhost:8443/jam/*"]'
 KEYCLOAK_SECRET=yHi6W2raPmLvPXoxqMA7VWbLAA2WN0eB
 create_client
+
+# Wildfly Elytron client legacy adapter fix
+/opt/keycloak/bin/kcadm.sh update clients/${KEYCLOAK_CLIENT_NAME} -r $KEYCLOAK_REALM -s 'attributes."exclude.issuer.from.auth.response"=true'
